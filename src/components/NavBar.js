9732f536c3548b8b59749f8f70d88dd6a39c6945
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-11-10 15:58:13 +0100

    Revert "Updated Login Component"
    This reverts commit d892083c22d3c139147715eaa4fcc34b7641c536.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,41 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import React from "react"
+import { NavLink } from "react-router-dom"
+import './NavBar.css';
 
-function NavBar({ loginMsg, isLoggedIn }) {
-  return (
-    <ul className="header">
-      <li>
-        <NavLink exact activeClassName="active" to="/">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName="active" to="/jokes">
-          Jokes
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName="active" to="/quotes">
-          Quotes
-        </NavLink>
-      </li>
-      {isLoggedIn && (
+function NavBar({loginMsg, isLoggedIn}) {
+    return (
+      <ul className="header">
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/jokes">
+            Jokes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/quotes">
+            Quotes
+          </NavLink>
+        </li>
+        {isLoggedIn && (
         <>
-          <li>
-            <NavLink activeClassName="active" to="/manage-jokes">
-              Manage Jokes
-            </NavLink>
-          </li>
+        <li> 
+          <NavLink activeClassName="active" to="/manage-jokes">
+            Manage Jokes
+          </NavLink>
+        </li>
         </>
-      )}
-      <li>
-        <NavLink activeClassName="active" to="/login-out">
-          {loginMsg}
-        </NavLink>
-      </li>
-    </ul>
-  );
-}
+        )}
+        <li>
+          <NavLink activeClassName="active" to="/login-out">
+            {loginMsg}
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
 
-export default NavBar;
+export default NavBar
